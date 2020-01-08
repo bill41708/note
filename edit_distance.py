e19fa25b30c8d0842bdeb658130402cb2b94aa4e
bill41708 <33356634+bill41708@users.noreply.github.com>
2020-01-08 22:26:14 +0800

    Add files via upload

--- edit_distance.py
+++ edit_distance.py
@@ -1,30 +1,23 @@
-
 filepath = './dictionary.txt'
 
 file = open(filepath, "r")
 
 #temp = file.readline().splitlines()
 
-def levenshteinDistance(s1, s2):
+def LevenshteinDistance(s1, s2):
     if len(s1) > len(s2):
         s1, s2 = s2, s1
 
     distances = range(len(s1) + 1)
-    for i2, c2 in enumerate(s2):
-        distances_ = [i2+1]
-        for i1, c1 in enumerate(s1):
+    for j, c2 in enumerate(s2):
+        distances_ = [j+1]
+        for i, c1 in enumerate(s1):
             if c1 == c2:
-                distances_.append(distances[i1])
+                distances_.append(distances[i])
             else:
-                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+                distances_.append(1 + min((distances[i], distances[i + 1], distances_[-1])))
         distances = distances_
     return distances[-1]
-
-'''
-s1 = 'apple'
-s2 = 'apple'
-print(levenshteinDistance(s1,s2))
-'''
 
 
 while True:
@@ -35,15 +28,6 @@ while True:
 	list = [[],[],[]]
 	same = []
 	flag = 0
-	input1 = 'a'
-	'''
-	print("init")
-	print(flag)
-	print(list[0])
-	print(list[1])
-	print(list[2])
-	'''
-
 
 
 	input1 = input('Input the word you are looking for(0 exit)：')
@@ -56,10 +40,7 @@ while True:
 		#print(temp)
 		#print(len(temp))
 
-		#temp = temp.splitlines()
-
-
-		a = levenshteinDistance(input1,temp[:-1])
+		a = LevenshteinDistance(input1,temp[:-1])
 		if a == 0:
 			same.append(temp)
 			flag = 1;
@@ -88,16 +69,5 @@ while True:
 			print("edit_distance" + str(j+1) + ":")
 			for i in range(len(list[j])):
 				print(list[j][i])
-	
-		
 
 	file.close()
-
-
-
-
-
-
-
-
-
